16defc23f62970832e9cb9537494a9ceb5f1159a
Fred C <github-fred@hidzz.com>
2021-10-07 13:37:53 -0700

    Automatic times update

--- flash.sh
+++ flash.sh
@@ -8,24 +8,27 @@ delay() {
 
 ~/bin/cssmin -f html/style.css
 
-/opt/local/bin/ampy -d 1 put times.dat
-
 #delay && /opt/local/bin/ampy -d 1 mkdir lib
 #delay && /opt/local/bin/ampy -d 1 mkdir html
 #delay && /opt/local/bin/ampy -d 1 put html/pump-on.png html/pump-on.png
 #delay && /opt/local/bin/ampy -d 1 put html/pump-off.png html/pump-off.png
-#delay && /opt/local/bin/ampy -d 1 put main.py
-
-#delay && /opt/local/bin/ampy -d 1 put html/index.html html/index.html
-#delay && /opt/local/bin/ampy -d 1 put html/style.min.css html/style.min.css
-
 #mpy-cross lib/logging.py
 #delay && /opt/local/bin/ampy -d 1 put lib/logging.mpy lib/logging.mpy
-
 #mpy-cross -v wificonfig.py
 #delay && /opt/local/bin/ampy -d 1 put wificonfig.mpy
 
+delay && /opt/local/bin/ampy -d 1 put main.py
+if [[ -f update.py ]]; then
+    delay && /opt/local/bin/ampy -d 1 put update.py
+fi
+
+delay && /opt/local/bin/ampy -d 1 put html/index.html html/index.html
+delay && /opt/local/bin/ampy -d 1 put html/style.min.css html/style.min.css
+
 mpy-cross -v belkin.py
 delay && /opt/local/bin/ampy -d 1 put belkin.mpy
+
+/opt/local/bin/ampy -d 1 put times.dat
+
 
 # delay && /opt/local/bin/ampy -d 1 ls
--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 #
+try:
+  import update
+  update.timesfile()
+except:
+  print("Update error")
+  pass
+
 import belkin
 belkin.main()
